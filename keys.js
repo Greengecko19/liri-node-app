9503276c37251598a797d71cbb52ef0b81e41f6b
Kamran <kfg09@yahoo.com>
2018-04-20 12:15:32 -0400

    Initial push

---
+++ keys.js
@@ -0,0 +1,14 @@
+
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.B6UuVp6VfK7gW640WjXbsxeEU,
+  consumer_secret: process.env.WQj4c88fTHycdWtfj4rN2y8qWa1KqxQloGI4lK2WNLarFIPBi1,
+  access_token_key: process.env.986692255405740032-6lQVAmKGhh0aKihv0RE0gyJz6Loy14z,
+  access_token_secret: process.env.A27l7sqDt4blU6xAIzeUS9wqRzntxoULfQIQd4wRKygOH
+};
+
+exports.spotify = {
+  id: process.env.2d48d822682648a193e84da2d44bda58,
+  secret: process.env.ccbc09d1389e4364a048dcc5f85a9f44
+};
