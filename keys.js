80a179b050625593e7900205ed036e47cedb69bf
Kamran Grasselli <kfg09@yahoo.com>
2018-04-21 23:21:12 -0400

    Finally got the package to read right

--- keys.js
+++ keys.js
@@ -12,3 +12,5 @@ module.exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+
--- liri.js
+++ liri.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
+var keys = require('./keys.js');
+// console.log(keys);
 
-var apiKeys = require('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
-var spotifier = new Spotify(apiKeys.spotify);
-var tweeter = new Twitter(apiKeys.twitter);
+var spotifier = new Spotify(keys.spotify);
+var tweeter = new Twitter(keys.twitter);
 
 console.log(spotifier);
 console.log(tweeter);
