c169c0957148712a2f17ad1dfe5b7ccdc9d6a48e
Kamran <kfg09@yahoo.com>
2018-04-25 17:13:54 -0400

    Figured out how to display album info

--- liri.js
+++ liri.js
@@ -33,22 +33,25 @@ mainSelect(process.argv[2], process.argv[3]);
 function spotifyThisSong(songName) {
     spotify.search({
         type: 'track',
-        query: songName,
-        limit: 20
+        query: songName
+        // limit: 2
     },
     function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-               
-        var songs = data.tracks.items;
-        for (var i = 0; i < songs.length; i++) {
+        
+        // console.log(JSON.stringify(data, null, 2));
+        
+        var results = data.tracks.items;
+        for (var i = 0; i < results.length; i++) {
             
-            for (var j=0; j < songs[i].artists.length; j++) {
-                console.log(i + "  Artist: " + songs[i].artists[j].name);
+            for (var j=0; j < results[i].artists.length; j++) {
+                console.log(i + "  Artist: " + results[i].artists[j].name +
+                " --  Album: " + results[i].album.name);
             }
             
-            // console.log(i + '  Artist: ' + songs[i].artists.map ( artist => { return artist.name }));  // alternate mapping method
+            //  console.log(i + '  Artist: ' + results[i].artists.map ( artist => { return artist.name }));  // alternate mapping method
         }
     });
 };
